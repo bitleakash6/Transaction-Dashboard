db5c0b48f9706172b2e1e43c658efec3a7bba6a3
Akash Bitle <bitleakash6@gmail.com>
2024-06-20 11:56:53 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const apiRoutes = require('./routes/api');
+const mongoConnect = require('./db/mongoConnect');
+
+const app = express();
+
+mongoConnect();
+app.use(express.json());
+app.use(cors());
+
+app.use('/', apiRoutes);
+// app.get('/', (req, res) =>{   test purpose
+//     res.send("Hello world!");
+// })
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
